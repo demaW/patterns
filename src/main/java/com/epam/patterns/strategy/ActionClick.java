3edf7d65722c8037af6069997442b89c3acacdaf
demaW <demavov@gmail.com>
2015-02-26 00:19:31 +0200

    init

---
+++ src/main/java/com/epam/patterns/strategy/ActionClick.java
@@ -0,0 +1,23 @@
+package com.epam.patterns.strategy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ActionClick {
+    private ClickingStrategy clickingStrategy = new Click();
+    private WebDriver driver;
+    private By locator;
+
+    public ActionClick(WebDriver driver, By locator) {
+        this.driver = driver;
+        this.locator = locator;
+    }
+
+    public void performClick(){
+        clickingStrategy.clickButton(this.driver,this.locator);
+    }
+
+    public void changeStrategy(ClickingStrategy clickingStrategyNew){
+        clickingStrategy = clickingStrategyNew;
+    }
+}
