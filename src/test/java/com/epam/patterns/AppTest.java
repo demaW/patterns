ed27e77af0524cf2beb4b536bf296be4659247b2
demaW <demavov@gmail.com>
2015-03-01 20:57:46 +0200

    merge issue  Please enter the commit message for your changes. Lines starting

--- src/test/java/com/epam/patterns/AppTest.java
+++ src/test/java/com/epam/patterns/AppTest.java
@@ -1,12 +1,17 @@
 package com.epam.patterns;
 
 
-import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest{
 
+
+    @AfterTest
+    public void tearDown(){
+        System.out.println("end");
+    }
 }
 
