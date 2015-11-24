9778cf2684cd5ede542dd7bb57fb78bc54adbf84
demaW <demavov@gmail.com>
2015-11-24 23:41:59 +0200

    added methods call to main method

--- src/main/java/com/epam/patterns/facade/FacadeMain.java
+++ src/main/java/com/epam/patterns/facade/FacadeMain.java
@@ -2,6 +2,9 @@ package com.epam.patterns.facade;
 
 public class FacadeMain {
     public static void main(String[] args){
+        ShopsFacade shopsFacade = new ShopsFacade();
 
+        System.out.println("When I go for regular shopping I will spend: " + shopsFacade.goForRegularShoppin() + " $");
+        System.out.println("When I go for shopping to Grocery an Sport Shop I will spend: " + shopsFacade.goForShoppingAndSportGoods() + " $");
     }
 }
